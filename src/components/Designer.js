a9af472cf6fdb43b2267afb9471ced5f8150e3b9
Drew Anderson <drew@mohdi.com>
2024-06-10 13:34:01 -0700

    copy fix

--- src/components/Designer.js
+++ src/components/Designer.js
@@ -650,7 +650,7 @@ transform: "translate(-50%, -50%)"
           >
             Download
           </button>
-            <h6 style={{ fontSize: '8px' }}>cause Halloween doesn't last all year</h6>
+          
           <ItemBlock src="/img/layers/accessories/5.png" width="100" height="100" ></ItemBlock>
         </div>
   </div>
